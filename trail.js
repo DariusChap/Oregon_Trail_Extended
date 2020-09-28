2cbae4d2258d2ec2a118ea585aa578909a125392
Darius <chapd289@robertmorris.edu>
2020-09-28 14:55:23 -0500

    final code

--- trail.js
+++ trail.js
@@ -2,23 +2,23 @@ class Traveler {
     constructor(name) {
         this.name = name
         this.food = 1
-        this.isHealthy = true 
+        this.isHealthy = true
 
     }
 
-    hunt () {
-        this.food += 2 
+    hunt() {
+        return this.food = this.food + 2
 
     }
-    eat () {
-        if(this.food > 0) {
-            this.food -= 1;
-        } else {this.isHealthy = false}
-   
-    
+    eat() {
+        if (this.food >= 1) {
+            this.food = this.food - 1
+        } else { this.isHealthy = false }
+
+
     }
 
-}    
+}
 
 class Wagon {
     constructor(capacity) {
@@ -26,68 +26,72 @@ class Wagon {
         this.capacity = capacity
     }
 
-    getAvailableSeatCount () {
-         return this.capacity - this.passengers.length
-        
+    getAvailableSeatCount() {
+        return this.capacity - this.passengers.length
+
 
     }
-    
+
     join(traveler) {
-        if(this.getAvailableSeatCount() > 0) {
+        if (this.getAvailableSeatCount() > 0) {
             this.passengers.push(traveler)
-        } 
+        }
     }
 
     shouldQuarantine() {
-       return this.passengers.some(function (passenger) {
-           return passenger.isHealthy === false
-       }) 
+        return this.passengers.some(function (passenger) {
+            return passenger.isHealthy === false
+        })
 
     }
 
     totalFood() {
         return this.passengers.reduce(function (total, passenger) {
-          return total += passenger.food
+            return total += passenger.food
         }, 0)
     }
 
-        
+
 }
 
 class Doctor extends Traveler {
     constructor(name, food, isHealthy,) {
         super(name, food, isHealthy)
-        this.isHealthy = true
+    }
+
+    
+    heal(traveler) {
+       return traveler.isHealthy = true
     }
     
-
-    heal(traveler) {
-        return traveler.isHealthy = true
-    }
-        
-        
+    
 }
 
 class Hunter extends Traveler {
     constructor(name, food, isHealthy) {
         super(name, food, isHealthy)
         this.food = 2
+        this.isHealthy = true
     }
 
     hunt() {
-        this.food += 5
+        return this.food = this.food + 5
     }
 
     eat() {
-        if(this.food > 2) {
-            this.food -= 2
-        } else {this.isHealthy === false}
+        if (this.food >= 2) {
+            this.food = this.food - 2
+        } else { this.food = 0, this.isHealthy = false }
     }
 
     giveFood(traveler, numOfFoodUnits) {
-        if(this.numOfFoodUnits = 2) {
-            this.food += 1
-        } else {this.traveler = 0}
+        if(this.food > numOfFoodUnits) {
+            traveler.food += numOfFoodUnits
+            this.food -= numOfFoodUnits
+        }   
     }
-        
+   
+
+
+
 }
